623f2d05062e26eaa139e93c639b86ebe326e8e0
Yu-zq <2537080897@qq.com>
2021-01-27 21:41:25 +0800

    安装了element-ui

---
+++ src/plugins/element.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import { Button } from "element-ui";
+import { Header, Footer, Main, Aside, Container } from "element-ui";
+
+Vue.use(Button);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Footer);
+Vue.use(Main);
+Vue.use(Aside);
